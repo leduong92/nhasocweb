0b5255393bae0b289f5d67a89c8aa8fed75b8e20
macbook <mai.le.duong@scancom.net>
2023-08-20 19:20:40 +0700

    nav bả

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+    return <div className="border-t">Footer</div>;
 };
 
 export default Footer;
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,15 +1,22 @@
-import React from "react";
-import NavLink from "./nav-link";
+import React from 'react';
+import NavLink from './nav-link';
 
 const NavBar = () => {
-  return (
-    <header className="border-b p-4">
-      <nav className="space-x-4">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/products">Products</NavLink>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="border-b p-4">
+            <nav className="flex justify-between space-x-4">
+                <div>
+                    <NavLink href="/">Nha Soc Store</NavLink>
+                </div>
+
+                <div className="flex gap-2">
+                    <NavLink href="/">Trang chủ</NavLink>
+                    <NavLink href="/products">Sản phẩm</NavLink>
+                    <NavLink href="/about-us">About Us</NavLink>
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default NavBar;
