2e3ae0b921d4dbc2f7cba1604d1bae59d2a17259
Duong <ltson@theodorealexander.com>
2023-08-31 16:50:49 +0700

    product card

--- components/featured/featured.tsx
+++ components/featured/featured.tsx
@@ -67,43 +67,30 @@ const Featured = ({ products }: { products: IProduct[] }) => {
                 </div>
                 <div className='flex w-full z-0 gap-5 transition translate-x-0 ease-in-out duration-700 delay-100 scroll-smooth' style={{ transform: `translateX(${currentSlide}px)` }} >
                     {products.map((p, idx) => (
-                        <>
+                        <div ref={listRef} key={p.id}
+                            className=' relative flex flex-col p-2 rounded border group hover:scale-105 transition-transform ease-out duration-200 shadow-sm cursor-pointer'
+                        >
+                            <div className='w-full relative flex flex-row justify-between pb-2 z-10'>
+                                <span className='text-sm italic  top-2 border w-max rounded-full p-1 bg-sky-200' >Giảm {(p.originalPrice - p.price) / 1000}% </span>
+                            </div>
+                            <Link href={`/product-detail/${p.id}`} className='h-96 flex flex-col p-2'>
+                                <div className="relative w-[265px] max-h-64 flex-1 z-0">
+                                    <ProductImage product={p} fill />
+                                </div>
+                                <div className="flex flex-col items-center justify-between mb-4 gap-4">
+                                    <p className="text-[20px] w-full ">{p.metaTitle}</p>
 
-                            <div ref={listRef} key={p.id}
-                                className=' relative flex flex-col p-2 rounded border group hover:scale-105 transition-transform ease-out duration-200 shadow-sm cursor-pointer'
-                            >
-                                <div className='w-full relative flex flex-row justify-between pb-2 z-10'>
-                                    <span className='text-sm italic  top-2 border w-max rounded-full p-1 bg-sky-200' >Giảm {(p.originalPrice - p.price) / 1000}% </span>
-                                    <div className='flex flex-col gap-3 absolute right-3 '>
-                                        <button className='mr-5 invisible transition transform translate-x-12 duration-200 hover:scale-105 group-hover:translate-x-0  ease-out group-hover:visible'>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                                            </svg>
-                                        </button>
-                                        <button className='mr-5 invisible transition transform translate-x-12 duration-200 hover:scale-105 group-hover:translate-x-0  ease-out group-hover:visible'>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                            </svg>
-
-                                        </button>
+                                    <div className="w-full flex justify-between">
+                                        <p className='text-sky-400 font-bold text-lg '>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(p.originalPrice)}</p>
+                                        <p className='italic font-mono line-through'>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(p.price)}</p>
                                     </div>
                                 </div>
-                                <Link href={`/product-detail/${p.id}`} className='h-96 flex flex-col p-2'>
-                                    <div className="relative w-[265px] max-h-64 flex-1 z-0">
-                                        <ProductImage product={p} fill />
-                                    </div>
-                                    <div className="flex flex-col items-center justify-between mb-4 gap-4">
-                                        <p className="text-[20px] w-full ">{p.metaTitle}</p>
+                                <div className='w-full'>
+                                    <button className='w-full h-9 bg-sky-300 hover:bg-sky-400 transition-all transform duration-300 delay-100 text-white active:ring-2'>Thêm giỏ hàng</button>
+                                </div>
+                            </Link>
 
-                                        <div className="w-full flex justify-between">
-                                            <p className='text-sky-400 font-bold text-lg '>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(p.originalPrice)}</p>
-                                            <p className='italic font-mono line-through'>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(p.price)}</p>
-                                        </div>
-                                    </div>
-                                </Link>
-
-                            </div>
-                        </>
+                        </div>
                     ))}
                 </div>
 
--- components/product-card/product-card.tsx
+++ components/product-card/product-card.tsx
@@ -8,19 +8,30 @@ const ProductCard = ({ products }: { products: IProduct[] }) => {
         <>
             <div className='grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-8 gap-x-3 my-4'>
                 {products.map((p) => (
-                    <Link href={`/product-detail/${p.id}`} key={p.id} className='h-96 flex flex-col p-2 rounded border group hover:scale-105 transition-transform ease-out duration-200 shadow-sm cursor-pointer '>
-                        <div className="relative max-h-72 flex flex-1">
-                            <ProductImage product={p} fill />
+                    <div key={p.id}
+                        className=' relative flex flex-col p-2 rounded border group hover:scale-105 transition-transform ease-out duration-200 shadow-sm cursor-pointer'
+                    >
+                        <div className='w-full relative flex flex-row justify-between pb-2 z-10'>
+                            <span className='text-sm italic  top-2 border w-max rounded-full p-1 bg-sky-200' >Giảm {(p.originalPrice - p.price) / 1000}% </span>
                         </div>
-                        <div className="font-semibold flex items-center justify-between mt-4 mb-1">
-                            <p className="w-44 truncate">{p.metaTitle}</p>
-                        </div>
-                        <p className="italic text-xs w-32 line-clamp-2 text-gray-600 truncate ...">
-                            <p>${p.price}</p>
-                            {p.description}
-                        </p>
+                        <Link href={`/product-detail/${p.id}`} className='h-96 flex flex-col p-2'>
+                            <div className="relative  max-h-64 flex-1 z-0">
+                                <ProductImage product={p} fill />
+                            </div>
+                            <div className="flex flex-col items-center justify-between mb-4 gap-4">
+                                <p className="text-[20px] w-full ">{p.metaTitle}</p>
 
-                    </Link>
+                                <div className="w-full flex justify-between">
+                                    <p className='text-sky-400 font-bold text-lg '>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(p.originalPrice)}</p>
+                                    <p className='italic font-mono line-through'>{new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(p.price)}</p>
+                                </div>
+                            </div>
+                            <div className='w-full'>
+                                <button className='w-full h-9 bg-sky-300 hover:bg-sky-400 transition-all transform duration-300 delay-100 text-white active:ring-2'>Thêm giỏ hàng</button>
+                            </div>
+                        </Link>
+
+                    </div>
                 ))}
             </div>
         </>
