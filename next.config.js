959f6bde44fe6cf45f64f23fbf1d59a0803fb131
Duong <ltson@theodorealexander.com>
2023-09-13 11:14:10 +0700

    config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '5000',
-            },
-            {
-                protocol: 'http',
-                hostname: '172.18.108.243',
-                port: '5046',
-            },
-            {
-                protocol: 'http',
-                hostname: 'api.1live.io.vn',
-                port: '5000',
-            },
-        ],
+        domains: ['api.1live.io.vn'],
     },
     env: {
         BASE_URL: 'http://api.1live.io.vn/api',
-        BASE_IMAGE_URL: 'http://api.1live.io.vn/',
+        BASE_IMAGE_URL: 'http://api.1live.io.vn',
     },
 };
 
