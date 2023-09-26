8ff6c3780e743cfc73eafe9c9c38dbba85b99dc5
Duong <ltson@theodorealexander.com>
2023-09-26 14:26:12 +0700

    fix related link

--- components/product-card/related-product.tsx
+++ components/product-card/related-product.tsx
@@ -24,7 +24,7 @@ const RelatedProducts = async () => {
                         key={product.id}
                         className="aspect-square w-full flex-none min-[475px]:w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5"
                     >
-                        <Link className="relative h-full w-full" href={`/product/${product.id}`}>
+                        <Link className="relative h-full w-full" href={`/product-detail/${product.id}`}>
                             <GridTileImage
                                 alt={product.metaTitle}
                                 label={{
